9b436e2aa54127061f0887570d7c686a599e9fda
Happytoad <happytoad42@gmail.com>
2018-09-06 17:17:58 +0700

    Gulpfile modified css-lint tests added index file moved out

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var gulpStylelint = require('gulp-stylelint');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var minify = require('gulp-csso');
@@ -27,19 +28,30 @@ gulp.task("style", function () {
 		.pipe(server.stream());
 });
 
+gulp.task("lint-css", function lintCssTask () {
+	return gulp.src("./build/styles/*.css")
+		.pipe(gulpStylelint({
+			reporters: [
+			{formatter: 'string', console: true}
+			]
+		}));
+
+});
+
 gulp.task("serve", function () {
 	server.init({
-		server: "./build"
+		server: "./",
+		port: 3000 
 	});
 
 	gulp.watch("dev/sass/**/*.sass", ["style"]);
-	gulp.watch("dev/*.html", ["html"])
+	gulp.watch("*.html", ["html"])
 		.on("change", server.reload);
 
 });
 
 gulp.task("images", function() {
-	return gulp.src("./img/**/*.{png, jpg, svg}")
+	return gulp.src("dev/img/**/*.{png, jpg, svg}")
 		.pipe(imagemin([
 			imagemin.optipng({optimizationLevel: 3}),
 			imagemin.jpegtran({progressive: true}),
@@ -49,7 +61,7 @@ gulp.task("images", function() {
 });
 
 gulp.task("webp", function() {
-	return gulp.src("./img/**/*.{png, jpg}")
+	return gulp.src("dev/img/**/*.{png, jpg}")
 		.pipe(webp({quality: 90}))
 		.pipe(gulp.dest("./build/img"));
 });
@@ -57,7 +69,7 @@ gulp.task("webp", function() {
 gulp.task("html", function() {
 	return gulp.src("dev/*.html")
 		.pipe(posthtml())
-		.pipe(gulp.dest("./build"));
+		.pipe(gulp.dest("./"));
 });
 
 gulp.task("copy", function() {
